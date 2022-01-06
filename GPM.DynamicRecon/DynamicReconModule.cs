7b2c0271355375151ab27a157ab97d21b98375f5
Jim Payne <jim.payne@ametek.com>
2022-01-05 18:10:31 -0600

    Add initial project source

---
+++ GPM.DynamicRecon/DynamicReconModule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Cameca.CustomAnalysis.Interface.CustomAnalysis;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace GPM.CustomAnalysis.DynamicRecon
+{
+    [ModuleDependency("IvasModule")]
+    public class DynamicReconModule : IModule
+    {
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any additional dependencies with the Unity IoC container
+        }
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var customAnalysisService = containerProvider.Resolve<ICustomAnalysisService>();
+
+            customAnalysisService.Register<DynamicReconCustomAnalysis, DynamicReconOptions>(
+                new CustomAnalysisDescription("GPM_DynamicRecon", "GPM Dynamic Recon Params", new Version()));
+        }
+    }
+}
